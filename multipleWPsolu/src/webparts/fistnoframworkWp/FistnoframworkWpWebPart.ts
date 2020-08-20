71630ecccf27ea1b95fdca90bf989935ce69c502
Mengfei Kong <mengfeik@wicresoft.com>
2020-08-20 11:04:31 +0800

    Update FistnoframworkWpWebPart.ts

--- multipleWPsolu/src/webparts/fistnoframworkWp/FistnoframworkWpWebPart.ts
+++ multipleWPsolu/src/webparts/fistnoframworkWp/FistnoframworkWpWebPart.ts
@@ -13,19 +13,19 @@ export interface IFistnoframworkWpWebPartProps {
   description: string;
 }
 
-export default class FistnoframworkWpWebPart extends BaseClientSideWebPart <IFistnoframworkWpWebPartProps> {
+export default class FistnoframworkWpWebPart extends BaseClientSideWebPart<IFistnoframworkWpWebPartProps> {
 
   public render(): void {
     this.domElement.innerHTML = `
-      <div class="${ styles.fistnoframworkWp }">
-    <div class="${ styles.container }">
-      <div class="${ styles.row }">
-        <div class="${ styles.column }">
-          <span class="${ styles.title }">Welcome to SharePoint!</span>
-  <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
-    <p class="${ styles.description }">${escape(this.properties.description)}</p>
-      <a href="https://aka.ms/spfx" class="${ styles.button }">
-        <span class="${ styles.label }">Learn more</span>
+      <div class="${ styles.fistnoframworkWp}">
+    <div class="${ styles.container}">
+      <div class="${ styles.row}">
+        <div class="${ styles.column}">
+          <span class="${ styles.title}">Welcome to SharePoint!</span>
+  <p class="${ styles.subTitle}">Customize SharePoint experiences using Web Parts.</p>
+    <p class="${ styles.description}">${escape(this.properties.description)}</p>
+      <a href="https://aka.ms/spfx" class="${ styles.button}">
+        <span class="${ styles.label}">Learn more</span>
           </a>
           </div>
           </div>
@@ -34,28 +34,28 @@ export default class FistnoframworkWpWebPart extends BaseClientSideWebPart <IFis
   }
 
   protected get dataVersion(): Version {
-  return Version.parse('1.0');
-}
+    return Version.parse('1.0');
+  }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
-  return {
-    pages: [
-      {
-        header: {
-          description: strings.PropertyPaneDescription
-        },
-        groups: [
-          {
-            groupName: strings.BasicGroupName,
-            groupFields: [
-              PropertyPaneTextField('description', {
-                label: strings.DescriptionFieldLabel
-              })
-            ]
-          }
-        ]
-      }
-    ]
-  };
-}
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
 }
